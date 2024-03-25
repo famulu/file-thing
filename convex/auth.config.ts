175e2a5acc3398bed39eda98d6e737187bf00b1b
Mohammad Chohan <mohammadchohan6f@gmail.com>
2024-03-26 01:54:28 +1100

    set / as public route with clerk

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,8 +1,8 @@
 export default {
-    providers: [
-        {
-            domain: "https://one-coral-67.clerk.accounts.dev",
-            applicationID: "convex"
-        }
-    ]
-}
+  providers: [
+    {
+      domain: "https://one-coral-67.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
